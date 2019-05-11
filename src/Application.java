d6ba76cc688d310d352221621d5e2101ee5ed6df
Fiona Tolliday <fionatolliday@hotmail.com>
2019-05-11 17:13:53 +1000

    colour class with constructor

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,29 @@
 public class Application {
 
     public static void main(String[] args){
-        
+
     }
 }
+
+
+//    I would like to know the radius and color of the circle.
+//    By default radius =1 ,
+//    color = red
+//    but creator should be able to modify the radius.
+//    The program should be able to output:
+//    the default values ,
+//    changed value of radius given by user and
+//    then calculate the area
+
+
+//objectives:
+//print the radius
+//print the area of the circle
+//print the colour of the circle
+//
+//no scanner input
+//hard code the value
+//
+//
+//        To clarify: print the radius, area of circle and colour each time.
+//        No scanner input. Just hard code the values.
---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+
+    double radius;
+    double colour;
+
+    public Circle(double radius, double colour){
+        this.radius = radius;
+        this.colour = colour;
+    }
+    
+}
